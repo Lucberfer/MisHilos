e892ce349d5014d1add2d7152b9145a1290e9564
Lucas <lucberfer@gmail.com>
2024-11-15 20:10:37 +0100

    Finalizacion de la clase Customer

--- MisHilos/src/Customer.java
+++ MisHilos/src/Customer.java
@@ -1,2 +1,20 @@
+import java.util.List;
+
 public class Customer {
+
+    private String name;
+    private List<String> products;
+
+    public Customer(String name, List<String> products){
+        this.name = name;
+        this.products = products;
+    }
+
+    public List<String> getProducts() {
+        return products;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
