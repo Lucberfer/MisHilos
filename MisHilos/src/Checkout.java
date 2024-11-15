5809017ee86e3f2c1d7becd6feb5744ca050418d
Lucas <lucberfer@gmail.com>
2024-11-15 20:52:24 +0100

    Finalizacion de la clase Main

--- MisHilos/src/Checkout.java
+++ MisHilos/src/Checkout.java
@@ -20,21 +20,21 @@ public class Checkout implements Runnable {
                 this.initialTime = System.currentTimeMillis();
                 processCustomer(customer);
                 long totalTime = System.currentTimeMillis() - this.initialTime;
-                System.out.println("Caja" + name + "terminó de procesar" + customer.getName() + "en" + totalTime + "ms");
+                System.out.println("Caja " + name + " terminó de procesar " + customer.getName() + " en " + totalTime + "ms");
             }
         } catch (InterruptedException e) {
-            System.out.println("Caja" + name + "se interrumpió.");
+            System.out.println("Caja " + name + " se interrumpió.");
             Thread.currentThread().interrupt();
         }
     }
 
     private void processCustomer(Customer customer) {
 
-        System.out.println("Caja" + name + "esta procesando a" + customer.getName());
+        System.out.println("Caja " + name + " esta procesando a " + customer.getName());
 
         for (String product : customer.getProducts()){
             long productStartTime = System.currentTimeMillis();
-            System.out.println("Caja" + name + "está escaneando el producto" + product + "de" + customer.getName());
+            System.out.println("Caja " + name + " está escaneando el producto " + product + " de " + customer.getName());
 
             try {
                 Thread.sleep(1000);
@@ -44,7 +44,7 @@ public class Checkout implements Runnable {
             }
 
             long productEndTime = System.currentTimeMillis();
-            System.out.println("El tiempo de procesar" + product + "ha sido de" + (productEndTime - productStartTime) + "ms");
+            System.out.println("El tiempo de procesar " + product + " ha sido de " + (productEndTime - productStartTime) + "ms");
         }
     }
 }
--- MisHilos/src/Main.java
+++ MisHilos/src/Main.java
@@ -1,3 +1,37 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Main {
 
+    public static void main(String[] args){
+
+        BlockingQueue<Customer> queue = new ArrayBlockingQueue<>(5);
+
+        Checkout checkout1 = new Checkout("1", queue);
+        Checkout checkout2 = new Checkout("2", queue);
+
+        Thread checkoutThread1 =  new Thread(checkout1);
+        Thread checkoutThread2 = new Thread(checkout2);
+
+        checkoutThread1.start();
+        checkoutThread2.start();
+
+        Customer customer1 = new Customer("Pepe", Arrays.asList("Leche", "Pan", "Huevos", "Yogurt", "Platano"));
+        Customer customer2 = new Customer("Juan", Arrays.asList("Café", "Oreos", "Manzanas", "Arroz", "Pechuga de pollo"));
+        Customer customer3 = new Customer("Ana", Arrays.asList("Azúcar", "Cerveza", "Cereales", "Hamburguesas", "Cebollas"));
+        Customer customer4 = new Customer("Marta", Arrays.asList("Aguacate", "Mantequilla", "Pimients", "Agua", "Zumo de manzana"));
+        Customer customer5 = new Customer("Rafa", Arrays.asList("Patatas", "Tomates", "Avellanas", "Dorada", "Peras"));
+
+        try {
+            queue.put(customer1);
+            queue.put(customer2);
+            queue.put(customer3);
+            queue.put(customer4);
+            queue.put(customer5);
+        } catch (InterruptedException e) {
+            System.out.println("Se ha interrumpido los hilos");
+        }
+    }
 }
